2b7c3d2f0bfbcef44ec8a97339cd6a6ffd392c42
Stoil Stoichev <stoilstoychev@gmail.com>
2016-11-06 20:30:49 +0200

    Initial commit

---
+++ SeleniumTemplateProject/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium;
+
+namespace SeleniumTemplateProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SearchWikipedia()
+        {
+            var title = "Integration testing";
+
+            // InternetExplorerDriver ieDriver
+            InternetExplorerDriver ie = new InternetExplorerDriver(Environment.CurrentDirectory);
+            ie.Navigate().GoToUrl("https://www.wikipedia.org/");
+            ie.FindElement(By.Id("searchInput")).SendKeys(title);
+            ie.FindElement(By.ClassName("pure-button-primary-progressive")).Click();
+            var heading = ie.FindElement(By.Id("firstHeading"));
+            Assert.AreEqual(heading.Text, title);
+            ie.Close();
+
+        }
+    }
+}
